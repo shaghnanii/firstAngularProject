ee2cc62151e4e4acb2365a875a2266ef570f0270
shaghnanii <shakilsak05@gmail.com>
2024-04-08 10:09:26 +0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {HeroesListComponent} from "./heroes-list/heroes-list.component";
+import {CrisisListComponent} from "./crisis-list/crisis-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'crisis', component: CrisisListComponent},
+  {path: 'heroes', component: HeroesListComponent},
+];
